3df60584ba6dcc9e3a6d3f4468e095348adce7b7
Marcelo <marcelodossantosmachado05@gmail.com>
2026-04-11 16:29:43 -0300

    Feat: Adding page for price

---
+++ preco/preco.js
@@ -0,0 +1,101 @@
+function menu() {
+  const menuToggle = document.getElementById("menu");
+  menuToggle.style.display =
+    menuToggle.style.display === "none" ? "block" : "none";
+}
+
+//LOGOUT
+const btnLogout = document.getElementById("logout");
+btnLogout.addEventListener("click", logout);
+
+function logout() {
+  localStorage.clear;
+
+  window.location.href = "auth/Auth.html";
+}
+//===============================================================================
+
+const urlPrice = "https://projeto-pricefy-backend.onrender.com/pricefy/price/";
+//===============================================================================
+
+//LOCAL STORAGE - Token AND idUser
+const idUser = localStorage.getItem("idUser");
+const token = localStorage.getItem("Token");
+//===============================================================================
+
+function allprice() {
+  fetch(`${urlPrice}listPrices/${idUser}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Sucesso");
+      const prices = Array.isArray(data)
+        ? data
+        : data.content || data.data || [];
+      renderprice(prices);
+    })
+    .catch((error) => console.error("Erro: ", error));
+}
+
+//DELETE price
+function deleteprice(idPrice) {
+  fetch(`${urlPrice}delete/${idPrice}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}` },
+  })
+    .then(() => {
+      console.log("Deletado com sucesso!");
+      allprice();
+    })
+    .catch((error) => console.error("Erro:", error));
+}
+
+const sectionPrice = document.getElementById("lista-preco");
+
+function renderprice(prices) {
+  sectionPrice.innerHTML = "";
+
+  if (!prices || prices.length === 0) {
+    listaContent.innerHTML = `<h1 style="text-align="center;">Você não possui nenhum preço cadastrado!</h1>`;
+    return;
+  }
+
+  prices.forEach((price) => {
+    const div = document.createElement("div");
+
+    div.classList.add("price-item");
+
+    div.innerHTML = `
+    <strong>Nome: ${price.product}</strong>
+    <strong>R$${price.vlProduct}</strong>
+    <strong>Mercado: ${price.market}</strong>
+    <span>
+    <button class="btn-delete-price" data-id="${price.idPrice}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+</svg>
+</button>
+</span>
+    `;
+    sectionPrice.appendChild(div);
+  });
+}
+
+sectionPrice.addEventListener("click", (event) => {
+  const button = event.target.closest(".btn-delete-price");
+
+  if (button) {
+    const idPrice = button.dataset.id;
+
+    console.log("idPrice: ", idPrice);
+
+    deleteprice(idPrice);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", allprice);
